9ac25b161fdeb378bf0f4f33852d2c4fe552e0d0
Cyrus D. Harrison <cyrush@edge205.llnl.gov>
2012-03-29 14:07:33 -0700

    propagate PYTHONPATH in run_flow_vpe_workspace.sh

--- run_flow_vpe_workspace.sh
+++ run_flow_vpe_workspace.sh
@@ -2,6 +2,6 @@
 WORKSPACE_SCRIPT=`pwd`/"$1"
 cd flow && visit -noconfig -nowin -cli -s setup.py build
 cd ../
-export PYTHONPATH=`pwd`/flow/build/lib/
+export PYTHONPATH=$PYTHONPATH:`pwd`/flow/build/lib/
 cd visit_flow_vpe && ./run_workspace.sh $WORKSPACE_SCRIPT
 cd ../
