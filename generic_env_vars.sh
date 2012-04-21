87fc4ae1285db3962ef6b83c4dd43435901933c5
Cyrus Harrison <cyglor@gmail.com>
2012-04-21 12:59:38 -0700

    added some generic helpers

---
+++ generic_env_vars.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export MODULE_DEST=`pwd`/py-site-packages/
+export PYTHONPATH="$MODULE_DEST/lib/python2.6/site-packages"
---
+++ generic_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# SET THIS TO YOUR OWN DEST DIR
+export MODULE_DEST=`pwd`/py-site-packages/
+# python path must include your module dir
+export PYTHONPATH="$MODULE_DEST/lib/python2.6/site-packages"
+visit -nowin -cli -s install_visit_pyopencl_support.py $MODULE_DEST
