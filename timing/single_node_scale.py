6afa5cf5d539d96253dd146e46eb92835c996180
Cyrus D. Harrison <cyrush@edge84.llnl.gov>
2012-05-09 14:22:12 -0700

    test full range of chunk sizes

--- timing/single_node_scale.py
+++ timing/single_node_scale.py
@@ -2,17 +2,17 @@
 import subprocess
 
 def par(rtype):
-    for cs  in range(1,2):
+    for cs  in range(1,11):
         cmd  = "python run_timings.py "
         cmd += "/p/lscratchc/cyrush/rt3d_export_center_chunk_size_%03d.visit " % (cs)
-        cmd += "%s 2  2 _par_test_chunk_size_%03d " % (rtype,cs)
+        cmd += "%s 10  2 _par_test_chunk_size_%03d " % (rtype,cs)
         subprocess.call(cmd,shell=True)
 
 def ser(rtype):
-    for cs  in range(1,2):
+    for cs  in range(1,11):
         cmd  = "python run_timings.py "
         cmd += "/p/lscratchc/cyrush/rt3d_export/rt3d_export_center_with_coords_col_%04d_chunk_size_%03d_idx_0000.vtk " % (cs-1,cs)
-        cmd += "%s 2  1 _ser_test_chunk_size_%03d " % (rtype,cs)
+        cmd += "%s 10  1 _ser_test_chunk_size_%03d " % (rtype,cs)
         subprocess.call(cmd,shell=True)
 
 if __name__ == "__main__":
