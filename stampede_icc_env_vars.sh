1f25332f26be2ae65f0903a3a560ce3741b02b39
Cyrus Harrison <cyrush@llnl.gov>
2012-12-12 18:46:24 -0600

    updates for stampede

---
+++ stampede_icc_env_vars.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export ROOT=`pwd`
+export MODULE_DEST=`pwd`/py-site-packages/
+export VISIT_LOC="/work/00401/pnav/visit/trunk/src"
+export OPENCL_LOC="/opt/apps/intel/opencl/3.0"
+export PATH=$PATH:$VISIT_LOC/bin:${OPENCL_LOC}/bin
+# python path must include your module dir
+export PYTHONPATH="$MODULE_DEST/lib/python2.6/site-packages"
+export MODULE_DEST=$ROOT/py-site-packages/
+export PYTHONPATH="$MODULE_DEST/lib/python2.6/site-packages"
+export PYTHONPATH="$PYTHONPATH:$ROOT/flow/build/lib/"
+export PYTHONPATH="$PYTHONPATH:$ROOT/visit_flow_vpe/build/lib/"
--- stampede_icc_setup.sh
+++ stampede_icc_setup.sh
@@ -2,11 +2,11 @@
 export MODULE_DEST=`pwd`/py-site-packages/
 export VISIT_LOC="/work/00401/pnav/visit"
 export OPENCL_LOC="/opt/apps/intel/opencl/3.0"
-export PATH=$PATH:$VISIT_LOC/bin:${OPENCL_LOC}/bin
+export PATH=$PATH:$VISIT_LOC/trunk/src/bin:${OPENCL_LOC}/bin
 # python path must include your module dir
 export PYTHONPATH="$MODULE_DEST/lib/python2.6/site-packages"
 export CFLAGS="-I${OPENCL_LOC}/include"
-export LDFLAGS="-L${OPENCL_LOC}/lib64 -lOpenCL -L${VISIT_LOC}/visit/python/2.6.4/linux-x86_64_icc/lib"
+export LDFLAGS="-L${OPENCL_LOC}/lib64 -L${VISIT_LOC}/visit/python/2.6.4/linux-x86_64_icc/lib"
 #export FCOMPILER="intelem"
 export BLAS="None"
 export LAPACK="None"
